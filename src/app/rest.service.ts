924736f00c874e521c5b9a8b3f0ee8c7d22e4771
AnkurMukherjee01 <mukherjeenkur@gmail.com>
2018-08-30 20:19:47 -0700

    mix protocol fix

--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -19,92 +19,92 @@ export class RestService {
  
     // Uses http.get() to load data from a single API endpoint
    login(loginDet:Login) {
-        return this.http.post('http://139.59.74.138/api/login',JSON.stringify(loginDet),httpOptions);
+        return this.http.post('https://139.59.74.138/api/login',JSON.stringify(loginDet),httpOptions);
     }
 
     signup(signUpDet:SignUp) {
         console.log(JSON.stringify(signUpDet));
-        return this.http.post('http://139.59.74.138/api/signup',JSON.stringify(signUpDet),httpOptions);
+        return this.http.post('https://139.59.74.138/api/signup',JSON.stringify(signUpDet),httpOptions);
     }
 
     mcq(email:String){
         console.log(JSON.stringify({"email":email}));
-        return this.http.post('http://139.59.74.138/api/mcq',JSON.stringify({"email":email}),httpOptions);
+        return this.http.post('https://139.59.74.138/api/mcq',JSON.stringify({"email":email}),httpOptions);
     }
 
     fetchPendingApproval(){
-        return this.http.get('http://139.59.74.138/api/fetchapproval',httpOptions)
+        return this.http.get('https://139.59.74.138/api/fetchapproval',httpOptions)
     }
 
     updatependingapproval(approvals:Approval[]){
         console.log(JSON.stringify({"approvals":approvals}));
-        return this.http.post('http://139.59.74.138/api/updatependingapproval',JSON.stringify({"approvals":approvals}),httpOptions);
+        return this.http.post('https://139.59.74.138/api/updatependingapproval',JSON.stringify({"approvals":approvals}),httpOptions);
     }
 
     createBulkLogin(excel:String){
         console.log(JSON.stringify({"excel":excel.split("base64,")[1]}));
-        return this.http.post('http://139.59.74.138/api/bulklogin',JSON.stringify({"excel":excel.split("base64,")[1]}),httpOptions);
+        return this.http.post('https://139.59.74.138/api/bulklogin',JSON.stringify({"excel":excel.split("base64,")[1]}),httpOptions);
     }
 
     changePassword(password:ChangepasswordModel){
         console.log(JSON.stringify(password));
-        return this.http.post('http://139.59.74.138/api/changePassword',JSON.stringify(password),httpOptions);
+        return this.http.post('https://139.59.74.138/api/changePassword',JSON.stringify(password),httpOptions);
     }
 
     createTest(createTest:CreateTestModel){
         console.log(JSON.stringify(createTest));
-        return this.http.post('http://139.59.74.138/api/createTest',JSON.stringify(createTest),httpOptions);
+        return this.http.post('https://139.59.74.138/api/createTest',JSON.stringify(createTest),httpOptions);
     }
 
     getTestName():Observable<any>{
-        return this.http.get('http://139.59.74.138/api/getTest',httpOptions);
+        return this.http.get('https://139.59.74.138/api/getTest',httpOptions);
     }
 
     getEmployee(type:String){
-        return this.http.post('http://139.59.74.138/api/getEmployeeName',JSON.stringify({type:type}),httpOptions);
+        return this.http.post('https://139.59.74.138/api/getEmployeeName',JSON.stringify({type:type}),httpOptions);
     }
 
     assignTest(email:String,test:String){
         console.log(JSON.stringify({email:email,test:test}));
-        return this.http.post('http://139.59.74.138/api/assigntest',JSON.stringify({email:email,test:test}),httpOptions);
+        return this.http.post('https://139.59.74.138/api/assigntest',JSON.stringify({email:email,test:test}),httpOptions);
     }
     deleteTest(test:String){
         console.log(JSON.stringify({test:test}));
-        return this.http.post('http://139.59.74.138/api/deletetest',JSON.stringify({test:test}),httpOptions);
+        return this.http.post('https://139.59.74.138/api/deletetest',JSON.stringify({test:test}),httpOptions);
     }
 
     fetchTestDet(testName:String){
         console.log(JSON.stringify({test:testName}));
-        return this.http.post('http://139.59.74.138/api/fetchmcqtest',JSON.stringify({testName:testName}),httpOptions);
+        return this.http.post('https://139.59.74.138/api/fetchmcqtest',JSON.stringify({testName:testName}),httpOptions);
     }
 
     saveTest(testName:String,assignedDate:Date){
         console.log(JSON.stringify({testName:testName,assignedDate:assignedDate}));
-        return this.http.post('http://139.59.74.138/api/savemcqtest',JSON.stringify({testName:testName,assignedDate:assignedDate}),httpOptions);
+        return this.http.post('https://139.59.74.138/api/savemcqtest',JSON.stringify({testName:testName,assignedDate:assignedDate}),httpOptions);
     }
 
     updateAnswer(answer:Answers,duration:String, testName:String,assignedDate:Date){
         console.log(JSON.stringify({answers:answer,duration:duration,testName:testName,assignedDate:assignedDate}));
-        return this.http.post('http://139.59.74.138/api/savemcqanswer',JSON.stringify({answers:answer,duration:duration,testName:testName,assignedDate:assignedDate}),httpOptions);
+        return this.http.post('https://139.59.74.138/api/savemcqanswer',JSON.stringify({answers:answer,duration:duration,testName:testName,assignedDate:assignedDate}),httpOptions);
     }
 
     assignBulkTest(excel:String){
         console.log(JSON.stringify({"excel":excel.split("base64,")[1]}));
-        return this.http.post('http://139.59.74.138/api/assignbulktest',JSON.stringify({"excel":excel.split("base64,")[1]}),httpOptions);
+        return this.http.post('https://139.59.74.138/api/assignbulktest',JSON.stringify({"excel":excel.split("base64,")[1]}),httpOptions);
     }
 
     getRank(testName:String){
         console.log(JSON.stringify({testName:testName}));
-        return this.http.post('http://139.59.74.138/api/getRank',JSON.stringify({testName:testName}),httpOptions);
+        return this.http.post('https://139.59.74.138/api/getRank',JSON.stringify({testName:testName}),httpOptions);
     }
 
     getBatchList(){
-        return this.http.get('http://139.59.74.138/api/getbatchname',httpOptions)
+        return this.http.get('https://139.59.74.138/api/getbatchname',httpOptions)
     }
 
     getUserDetails(batchName:String){
         console.log(JSON.stringify({batchName:batchName}));
-        return this.http.post('http://139.59.74.138/api/getuserdetails',JSON.stringify({batchName:batchName}),httpOptions);
+        return this.http.post('https://139.59.74.138/api/getuserdetails',JSON.stringify({batchName:batchName}),httpOptions);
     }
 }
 
